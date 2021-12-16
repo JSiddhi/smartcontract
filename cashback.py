f486b405bf1510b0e915be98437ed85862050ded
JSiddhi <96115143+JSiddhi@users.noreply.github.com>
2021-12-16 22:39:36 +0000

    Update cashback.py

--- cashback.py
+++ cashback.py
@@ -10,37 +10,37 @@ contract_address     = [0x25338e1e162a9d3f8F4be67633277d5DfcACa9Eb]
 wallet_private_key   = [db4f93c1ea15afb1db3f5ca216931597f0065fb0d730818c200727591ae97e]
 wallet_address       = [0xf6b15DF3BaC48e94C23CcA42bfC83f870455bCa8]
 
-w3 = (Web3(WebsocketProvider([https://mainnet.infura.io/v3/0c23c4378d26402fb5e8a2e5cd2d9c6a])))
+web = (Web3(WebsocketProvider([https://mainnet.infura.io/v3/0c23c4378d26402fb5e8a2e5cd2d9c6a])))
 
-w3.eth.enable_unaudited_features()
+web.eth.enable_unaudited_features()
 
-contract = w3.eth.contract(address = contract_address, abi = contract_abi.abi)
+contract = web.eth.contract(address = contract_address, abi = contract_abi.abi)
 
 def send_ether_to_contract(amount_in_ether):
 
-    amount_in_wei = w3.toWei(amount_in_ether,'ether');
+    amount_in_wei = web.toWei(amount_in_ether,'ether');
 
-    nonce = w3.eth.getTransactionCount(wallet_address)
+    nonce = web.eth.getTransactionCount(wallet_address)
 
     txn_dict = {
             'to': contract_address,
             'value': amount_in_wei,
             'gas': 2000000,
-            'gasPrice': w3.toWei('40', 'gwei'),
+            'gasPrice': web.toWei('40', 'gwei'),
             'nonce': nonce,
             'chainId': 3
     }
 
-    signed_txn = w3.eth.account.signTransaction(txn_dict, wallet_private_key)
+    signed_txn = web.eth.account.signTransaction(txn_dict, wallet_private_key)
 
-    txn_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    txn_hash = web.eth.sendRawTransaction(signed_txn.rawTransaction)
 
     txn_receipt = None
 
     count = 0
     while txn_receipt is None and (count < 30):
 
-        txn_receipt = w3.eth.getTransactionReceipt(txn_hash)
+        txn_receipt = web.eth.getTransactionReceipt(txn_hash)
 
         print(txn_receipt)
 
@@ -60,27 +60,27 @@ def check_whether_address_is_approved(address):
 
 def broadcast_an_opinion(covfefe):
 
-    nonce = w3.eth.getTransactionCount(wallet_address)
+    nonce = web.eth.getTransactionCount(wallet_address)
 
     txn_dict = contract.functions.broadcastOpinion(covfefe).buildTransaction({
         'chainId': 3,
         'gas': 140000,
-        'gasPrice': w3.toWei('40', 'gwei'),
+        'gasPrice': web.toWei('40', 'gwei'),
         'nonce': nonce,
     })
 
-    signed_txn = w3.eth.account.signTransaction(txn_dict, private_key=wallet_private_key)
+    signed_txn = web.eth.account.signTransaction(txn_dict, private_key=wallet_private_key)
 
-    result = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    result = web.eth.sendRawTransaction(signed_txn.rawTransaction)
 
-    tx_receipt = w3.eth.getTransactionReceipt(result)
+    tx_receipt = web.eth.getTransactionReceipt(result)
 
     count = 0
     while tx_receipt is None and (count < 30):
 
         time.sleep(10)
 
-        tx_receipt = w3.eth.getTransactionReceipt(result)
+        tx_receipt = web.eth.getTransactionReceipt(result)
 
         print(tx_receipt)
 
