99f9a9ba4c47e4669c9f1b6d2d5b3e47a9e7dfc0
JSiddhi <96115143+JSiddhi@users.noreply.github.com>
2021-12-17 01:47:38 +0000

    Update cashback.py

--- cashback.py
+++ cashback.py
@@ -1,3 +1,4 @@
+
 import json
 from web3 import Web3
 
@@ -7,7 +8,6 @@ web3 = Web3(Web3.HTTPProvider(infura_url))
 abi = json.loads('[{"constant": true,"inputs": [],"name": "get_output","outputs": [{"name": "","type": "string"}],"payable": false,"stateMutability": "view","type": "function}]')
 
 address = '0xf6b15DF3BaC48e94C23CcA42bfC83f870455bCa8'
-
 
 contract = web3.eth.contract(address=address, abi=abi)
 
