352731a6c75f9fdd929738ffcacbb2cc0bfd5ac5
JSiddhi <96115143+JSiddhi@users.noreply.github.com>
2021-12-17 01:30:10 +0000

    Update cashback.py

--- cashback.py
+++ cashback.py
@@ -1,59 +1,26 @@
-from solcx import install_solc
-install_solc(version='latest')
+import json
 from web3 import Web3
-from solcx import compile_source
 
-# Solidity source code
-pragma solidity >0.5.0;
+infura_url = 'https://mainnet.infura.io/v3/0c23c4378d26402fb5e8a2e5cd2d9c6a'
+web3 = Web3(Web3.HTTPProvider(infura_url))
 
-     contract Greeter {
-        string public greeting;
+abi = json.loads('[{"constant":true,"inputs":[],"name":"get_output","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}]')
 
-         constructor() public {
-             greeting = 'Hello';
-        }
+address = '0xf6b15DF3BaC48e94C23CcA42bfC83f870455bCa8'
 
-         function setGreeting(string memory _greeting) public {
-            greeting = _greeting;
-        }
 
-         function greet() view public returns (string memory) {
-             return greeting;
-        }
-     }
-  
-)
-# retrieve the contract interface
-contract_id, contract_interface = compiled_sol.popitem()
+contract = web3.eth.contract(address=address, abi=abi)
 
-# get bytecode / bin
-bytecode = contract_interface['bin']
+#need to put .call() at the end to call the smart contract
+totalSupply = contract.functions.totalSupply().call()
+print(" ")
+print("Reading the smart contract from etherium node....")
+#convert supply to Wei witch is 18 decimal places)
 
-# get abi
-abi = contract_interface['abi']
+print(" ")
+print('Total Supply: ', totalSupply/1000000)
+print(" ")
+print('Smart Contract Name: ', contract.functions.name().call())
+print(" ")
+print('Symbol: ', contract.functions.symbol().call())
 
-# web3.py instance
-w3 = Web3(Web3.EthereumTesterProvider())
-
-# set pre-funded account as sender
-w3.eth.default_account = w3.eth.accounts[0]
-    >>> Greeter = w3.eth.contract(abi=abi, bytecode=bytecode)
-
-# Submit the transaction that deploys the contract
->>> tx_hash = Greeter.constructor().transact()
-
-# Wait for the transaction to be mined, and get the transaction receipt
-tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-
-greeter = w3.eth.contract(
-address=tx_receipt.contractAddress,
-abi=abi
-)
-
-Greeter.functions.greet().call()
-'Hello'
-
-tx_hash = greeter.functions.setGreeting('Nihao').transact()
-tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-greeter.functions.greet().call()
-'Nihao'
