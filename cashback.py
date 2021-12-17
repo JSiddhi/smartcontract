5f74f77d2edd7c71c69609f9faf1b1a178a8aab5
JSiddhi <96115143+JSiddhi@users.noreply.github.com>
2021-12-17 01:33:43 +0000

    Update cashback.py

--- cashback.py
+++ cashback.py
@@ -4,7 +4,22 @@ from web3 import Web3
 infura_url = 'https://mainnet.infura.io/v3/0c23c4378d26402fb5e8a2e5cd2d9c6a'
 web3 = Web3(Web3.HTTPProvider(infura_url))
 
-abi = json.loads('[{"constant":true,"inputs":[],"name":"get_output","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}]')
+abi = json.loads('[
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "get_output",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]'
 
 address = '0xf6b15DF3BaC48e94C23CcA42bfC83f870455bCa8'
 
